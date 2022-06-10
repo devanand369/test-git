85f8ac1ba47b19abbc13395bb39282f304f003af
Devanand Yadav <devananraj2002@gmail.com>
2022-06-10 10:24:11 +0530

    some coditionals added to main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ print('This is main program.\n')
 
 for i in range(5):
     print('New Features added.\n')
+
+import random
+
+if random.random(0,100)/2 ==0:
+    print("Git\n")
+else:
+    print("Github\n")
+    
