d145c1ac7fc5f89f2da574e6d278ee5d8c5331d8
Devanand Yadav <devananraj2002@gmail.com>
2022-06-10 09:38:33 +0530

    some more added

--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 This is a simple python main program.
 """
 print('This is main program.\n')
+
+for i in range(5):
+    print('I am latest version\n')
+    
--- testgit.py
+++ testgit.py
@@ -3,5 +3,5 @@ This is a demo program for learning github to upload a repository
 from local to remote on Github.
 """
 for i in range(5):
-    print('Successfully uploaded the repo.')
+    print('Welcome to Git.')
     
